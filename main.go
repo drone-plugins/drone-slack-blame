4dec68327d268874c345f43d7de8eb5378ec63e1
Thomas Boerger <thomas@webhippie.de>
2019-03-01 00:54:01 +0100

    Drop renovate, proper version definition, unified jsonnet config (#39)
    * Drop renovate config, it is simply too noisy
    
    * Add a reliable version and integrate libsonnet definition
    
    * Unified documentation and readme
    
    * Drop import of unused fmt package
    
    * Fix manifest template helper

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/pkg/errors"
@@ -10,8 +9,7 @@ import (
 )
 
 var (
-	version = "0.0.0"
-	build   = "0"
+	version = "unknown"
 )
 
 func main() {
@@ -19,7 +17,7 @@ func main() {
 	app.Name = "slack blame plugin"
 	app.Usage = "slack blame plugin"
 	app.Action = run
-	app.Version = fmt.Sprintf("%s+%s", version, build)
+	app.Version = version
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
 			Name:   "token",
