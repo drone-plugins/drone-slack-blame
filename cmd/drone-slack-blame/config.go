7b889b99531f57de497827ff502ede9477adacda
Don <don.j.olmstead@gmail.com>
2019-12-18 12:18:09 -0800

    Add settings

--- cmd/drone-slack-blame/config.go
+++ cmd/drone-slack-blame/config.go
@@ -12,18 +12,106 @@ import (
 )
 
 const (
-	// Add all the flag names here as const strings.
+	tokenFlag   = "token"
+	channelFlag = "channel"
+	mappingFlag = "mapping"
+
+	successUsernameFlag         = "success.username"
+	successIconFlag             = "success.icon"
+	successTemplateFlag         = "success.template"
+	successImageAttachmentsFlag = "success.image-attachments"
+
+	failureUsernameFlag         = "failure.username"
+	failureIconFlag             = "failure.icon"
+	failureTemplateFlag         = "failure.template"
+	failureImageAttachmentsFlag = "failure.image-attachments"
+
+	defaultUsername = "drone"
+	defaultIcon     = ":drone:"
 )
 
 // settingsFlags has the cli.Flags for the plugin.Settings.
 func settingsFlags() []cli.Flag {
-	// Replace below with all the flags required for the plugin's specific
-	// settings.
-	return []cli.Flag{}
+	return []cli.Flag{
+		&cli.StringFlag{
+			Name:    tokenFlag,
+			Usage:   "slack access token",
+			EnvVars: []string{"PLUGIN_TOKEN,SLACK_TOKEN"},
+		},
+		&cli.StringFlag{
+			Name:    channelFlag,
+			Usage:   "slack channel",
+			EnvVars: []string{"PLUGIN_CHANNEL"},
+		},
+		&cli.StringFlag{
+			Name:    mappingFlag,
+			Usage:   "mapping of authors to slack users",
+			EnvVars: []string{"PLUGIN_MAPPING"},
+		},
+		&cli.StringFlag{
+			Name:    successUsernameFlag,
+			Usage:   "username for successful builds",
+			Value:   defaultUsername,
+			EnvVars: []string{"PLUGIN_SUCCESS_USERNAME"},
+		},
+		&cli.StringFlag{
+			Name:    successIconFlag,
+			Usage:   "icon for successful builds",
+			Value:   defaultIcon,
+			EnvVars: []string{"PLUGIN_SUCCESS_ICON"},
+		},
+		&cli.StringFlag{
+			Name:    successTemplateFlag,
+			Usage:   "template for successful builds",
+			EnvVars: []string{"PLUGIN_SUCCESS_TEMPLATE"},
+		},
+		&cli.StringSliceFlag{
+			Name:    successImageAttachmentsFlag,
+			Usage:   "image attachments for successful builds",
+			EnvVars: []string{"PLUGIN_SUCCESS_IMAGE_ATTACHMENTS"},
+		},
+		&cli.StringFlag{
+			Name:    failureUsernameFlag,
+			Usage:   "username for failed builds",
+			Value:   defaultUsername,
+			EnvVars: []string{"PLUGIN_FAILURE_USERNAME"},
+		},
+		&cli.StringFlag{
+			Name:    failureIconFlag,
+			Usage:   "icon for failed builds",
+			Value:   defaultIcon,
+			EnvVars: []string{"PLUGIN_FAILURE_ICON"},
+		},
+		&cli.StringFlag{
+			Name:    failureTemplateFlag,
+			Usage:   "template for failed builds",
+			EnvVars: []string{"PLUGIN_FAILURE_TEMPLATE"},
+		},
+		&cli.StringSliceFlag{
+			Name:    failureImageAttachmentsFlag,
+			Usage:   "image attachments for failed builds",
+			EnvVars: []string{"PLUGIN_FAILURE_IMAGE_ATTACHMENTS"},
+		},
+	}
 }
 
 // settingsFromContext creates a plugin.Settings from the cli.Context.
 func settingsFromContext(ctx *cli.Context) slackblame.Settings {
-	// Replace below with the parsing of the
-	return slackblame.Settings{}
+	return slackblame.Settings{
+		Token:   ctx.String(tokenFlag),
+		Channel: ctx.String(channelFlag),
+		Mapping: ctx.String(mappingFlag),
+		Success: slackblame.MessageOptions{
+			Username:         ctx.String(successUsernameFlag),
+			Icon:             ctx.String(successIconFlag),
+			Template:         ctx.String(successTemplateFlag),
+			ImageAttachments: ctx.StringSlice(successImageAttachmentsFlag),
+		},
+		Failure: slackblame.MessageOptions{
+			Username:         ctx.String(failureUsernameFlag),
+			Icon:             ctx.String(failureIconFlag),
+			Template:         ctx.String(failureTemplateFlag),
+			ImageAttachments: ctx.StringSlice(failureImageAttachmentsFlag),
+		},
+	}
 }
